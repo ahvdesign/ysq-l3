de4197f779e6be513621821dce1d164bc5fa60ec
Alexander Henriquez <ahvdesign@Alexanders-MBP.fios-router.home>
2019-06-13 03:48:54 -0400

    Results working

--- src/App.js
+++ src/App.js
@@ -198,6 +198,473 @@ class App extends Component {
     this.setState({ renderResults: true });
   };
 
+  findResult = schemaName => {
+    if (schemaName === 'emotionalDeprivation') {
+      if (
+        this.state.emotionalDeprivation.total >= 0 &&
+        this.state.emotionalDeprivation.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.emotionalDeprivation.total >= 9 &&
+        this.state.emotionalDeprivation.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.emotionalDeprivation.total >= 19 &&
+        this.state.emotionalDeprivation.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.emotionalDeprivation.total >= 31 &&
+        this.state.emotionalDeprivation.total <= 54
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'abandonment') {
+      if (
+        this.state.abandonment.total >= 0 &&
+        this.state.abandonment.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.abandonment.total >= 13 &&
+        this.state.abandonment.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.abandonment.total >= 26 &&
+        this.state.abandonment.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.abandonment.total >= 40 &&
+        this.state.abandonment.total <= 102
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'mistrustAbuse') {
+      if (
+        this.state.mistrustAbuse.total >= 0 &&
+        this.state.mistrustAbuse.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.mistrustAbuse.total >= 13 &&
+        this.state.mistrustAbuse.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.mistrustAbuse.total >= 26 &&
+        this.state.mistrustAbuse.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.mistrustAbuse.total >= 40 &&
+        this.state.mistrustAbuse.total <= 102
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'socialIsolation') {
+      if (
+        this.state.socialIsolation.total >= 0 &&
+        this.state.socialIsolation.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.socialIsolation.total >= 9 &&
+        this.state.socialIsolation.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.socialIsolation.total >= 19 &&
+        this.state.socialIsolation.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.socialIsolation.total >= 31 &&
+        this.state.socialIsolation.total <= 60
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'defectiveness') {
+      if (
+        this.state.defectiveness.total >= 0 &&
+        this.state.defectiveness.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.defectiveness.total >= 13 &&
+        this.state.defectiveness.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.defectiveness.total >= 26 &&
+        this.state.defectiveness.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.defectiveness.total >= 40 &&
+        this.state.defectiveness.total <= 90
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'failure') {
+      if (this.state.failure.total >= 0 && this.state.failure.total <= 8) {
+        return 'Low';
+      } else if (
+        this.state.failure.total >= 9 &&
+        this.state.failure.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.failure.total >= 19 &&
+        this.state.failure.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.failure.total >= 31 &&
+        this.state.failure.total <= 54
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'dependence') {
+      if (
+        this.state.dependence.total >= 0 &&
+        this.state.dependence.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.dependence.total >= 13 &&
+        this.state.dependence.total <= 35
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.dependence.total >= 26 &&
+        this.state.dependence.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.dependence.total >= 40 &&
+        this.state.dependence.total <= 90
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'vulnerability') {
+      if (
+        this.state.vulnerability.total >= 0 &&
+        this.state.vulnerability.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.vulnerability.total >= 9 &&
+        this.state.vulnerability.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.vulnerability.total >= 19 &&
+        this.state.vulnerability.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.vulnerability.total >= 31 &&
+        this.state.vulnerability.total <= 72
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'enmeshment') {
+      if (
+        this.state.enmeshment.total >= 0 &&
+        this.state.enmeshment.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.enmeshment.total >= 9 &&
+        this.state.enmeshment.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.enmeshment.total >= 19 &&
+        this.state.enmeshment.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.enmeshment.total >= 31 &&
+        this.state.enmeshment.total <= 66
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'subjugation') {
+      if (
+        this.state.subjugation.total >= 0 &&
+        this.state.subjugation.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.subjugation.total >= 9 &&
+        this.state.subjugation.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.subjugation.total >= 19 &&
+        this.state.subjugation.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.subjugation.total >= 31 &&
+        this.state.subjugation.total <= 60
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'selfSacrifice') {
+      if (
+        this.state.selfSacrifice.total >= 0 &&
+        this.state.selfSacrifice.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.selfSacrifice.total >= 13 &&
+        this.state.selfSacrifice.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.selfSacrifice.total >= 26 &&
+        this.state.selfSacrifice.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.selfSacrifice.total >= 40 &&
+        this.state.selfSacrifice.total <= 102
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'emotionalInhibition') {
+      if (
+        this.state.emotionalInhibition.total >= 0 &&
+        this.state.emotionalInhibition.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.emotionalInhibition.total >= 9 &&
+        this.state.emotionalInhibition.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.emotionalInhibition.total >= 19 &&
+        this.state.emotionalInhibition.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.emotionalInhibition.total >= 31 &&
+        this.state.emotionalInhibition.total <= 54
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'unrelentingStandards') {
+      if (
+        this.state.unrelentingStandards.total >= 0 &&
+        this.state.unrelentingStandards.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.unrelentingStandards.total >= 13 &&
+        this.state.unrelentingStandards.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.unrelentingStandards.total >= 26 &&
+        this.state.unrelentingStandards.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.unrelentingStandards.total >= 40 &&
+        this.state.unrelentingStandards.total <= 96
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'entitlement') {
+      if (
+        this.state.entitlement.total >= 0 &&
+        this.state.entitlement.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.entitlement.total >= 9 &&
+        this.state.entitlement.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.entitlement.total >= 19 &&
+        this.state.entitlement.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.entitlement.total >= 31 &&
+        this.state.entitlement.total <= 66
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'insufficientSelfControl') {
+      if (
+        this.state.insufficientSelfControl.total >= 0 &&
+        this.state.insufficientSelfControl.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.insufficientSelfControl.total >= 13 &&
+        this.state.insufficientSelfControl.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.insufficientSelfControl.total >= 26 &&
+        this.state.insufficientSelfControl.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.insufficientSelfControl.total >= 40 &&
+        this.state.insufficientSelfControl.total <= 90
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'approvalSeeking') {
+      if (
+        this.state.approvalSeeking.total >= 0 &&
+        this.state.approvalSeeking.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.approvalSeeking.total >= 13 &&
+        this.state.approvalSeeking.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.approvalSeeking.total >= 26 &&
+        this.state.approvalSeeking.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.approvalSeeking.total >= 40 &&
+        this.state.approvalSeeking.total <= 84
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'negativityPessimism') {
+      if (
+        this.state.negativityPessimism.total >= 0 &&
+        this.state.negativityPessimism.total <= 8
+      ) {
+        return 'Low';
+      } else if (
+        this.state.negativityPessimism.total >= 9 &&
+        this.state.negativityPessimism.total <= 18
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.negativityPessimism.total >= 19 &&
+        this.state.negativityPessimism.total <= 30
+      ) {
+        return 'High';
+      } else if (
+        this.state.negativityPessimism.total >= 31 &&
+        this.state.negativityPessimism.total <= 66
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+
+    if (schemaName === 'punitiveness') {
+      if (
+        this.state.punitiveness.total >= 0 &&
+        this.state.punitiveness.total <= 12
+      ) {
+        return 'Low';
+      } else if (
+        this.state.punitiveness.total >= 13 &&
+        this.state.punitiveness.total <= 25
+      ) {
+        return 'Medium';
+      } else if (
+        this.state.punitiveness.total >= 26 &&
+        this.state.punitiveness.total <= 39
+      ) {
+        return 'High';
+      } else if (
+        this.state.punitiveness.total >= 40 &&
+        this.state.punitiveness.total <= 84
+      ) {
+        return 'Very High';
+      } else {
+        return 'Error';
+      }
+    }
+  };
+
   render() {
     return (
       <div className="App">
@@ -221,7 +688,11 @@ class App extends Component {
         <button className="btn-submit" onClick={this.updateState}>
           Submit!
         </button>
-        {this.state.renderResults ? <Results data={this.state} /> : <p />}
+        {this.state.renderResults ? (
+          <Results data={this.state} findResult={this.findResult} />
+        ) : (
+          <p />
+        )}
       </div>
     );
   }
--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Results.scss';
 
-const Results = ({ data }) => {
+const Results = ({ data, findResult }) => {
   return (
     <div className="Results">
       <div className="results-row">
         <p className="schema-name">Schema Name</p>
         <p className="schema-code">Schema Code</p>
-        <p className="schema-count">Total Scored</p>
+        <p className="schema-count">Total Scored (Max Possible)</p>
         <p className="schema-count"># of 4s</p>
         <p className="schema-count"># of 5s</p>
         <p className="schema-count"># of 6s</p>
@@ -16,11 +16,166 @@ const Results = ({ data }) => {
       <div className="results-row">
         <p className="schema-name">Emotional Deprivation</p>
         <p className="schema-code">ED</p>
-        <p className="schema-count">{data.emotionalDeprivation.total}</p>
+        <p className="schema-count">{data.emotionalDeprivation.total} (54)</p>
         <p className="schema-count">{data.emotionalDeprivation.fours}</p>
         <p className="schema-count">{data.emotionalDeprivation.fives}</p>
         <p className="schema-count">{data.emotionalDeprivation.sixes}</p>
-        <p className="schema-result" />
+        <p className="schema-result">{findResult('emotionalDeprivation')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Abandonment</p>
+        <p className="schema-code">AB</p>
+        <p className="schema-count">{data.abandonment.total} (102)</p>
+        <p className="schema-count">{data.abandonment.fours}</p>
+        <p className="schema-count">{data.abandonment.fives}</p>
+        <p className="schema-count">{data.abandonment.sixes}</p>
+        <p className="schema-result">{findResult('abandonment')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Mistrust/Abuse</p>
+        <p className="schema-code">MA</p>
+        <p className="schema-count">{data.mistrustAbuse.total} (102)</p>
+        <p className="schema-count">{data.mistrustAbuse.fours}</p>
+        <p className="schema-count">{data.mistrustAbuse.fives}</p>
+        <p className="schema-count">{data.mistrustAbuse.sixes}</p>
+        <p className="schema-result">{findResult('mistrustAbuse')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Social Isolation</p>
+        <p className="schema-code">SI</p>
+        <p className="schema-count">{data.socialIsolation.total} (60)</p>
+        <p className="schema-count">{data.socialIsolation.fours}</p>
+        <p className="schema-count">{data.socialIsolation.fives}</p>
+        <p className="schema-count">{data.socialIsolation.sixes}</p>
+        <p className="schema-result">{findResult('socialIsolation')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Defectiveness</p>
+        <p className="schema-code">DS</p>
+        <p className="schema-count">{data.defectiveness.total} (90)</p>
+        <p className="schema-count">{data.defectiveness.fours}</p>
+        <p className="schema-count">{data.defectiveness.fives}</p>
+        <p className="schema-count">{data.defectiveness.sixes}</p>
+        <p className="schema-result">{findResult('defectiveness')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Failure</p>
+        <p className="schema-code">FA</p>
+        <p className="schema-count">{data.failure.total} (54)</p>
+        <p className="schema-count">{data.failure.fours}</p>
+        <p className="schema-count">{data.failure.fives}</p>
+        <p className="schema-count">{data.failure.sixes}</p>
+        <p className="schema-result">{findResult('failure')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Dependence</p>
+        <p className="schema-code">DI</p>
+        <p className="schema-count">{data.dependence.total}(90)</p>
+        <p className="schema-count">{data.dependence.fours}</p>
+        <p className="schema-count">{data.dependence.fives}</p>
+        <p className="schema-count">{data.dependence.sixes}</p>
+        <p className="schema-result">{findResult('dependence')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Vulnerability</p>
+        <p className="schema-code">VU</p>
+        <p className="schema-count">{data.vulnerability.total}(72)</p>
+        <p className="schema-count">{data.vulnerability.fours}</p>
+        <p className="schema-count">{data.vulnerability.fives}</p>
+        <p className="schema-count">{data.vulnerability.sixes}</p>
+        <p className="schema-result">{findResult('vulnerability')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Enmeshment</p>
+        <p className="schema-code">EU</p>
+        <p className="schema-count">{data.enmeshment.total}(66)</p>
+        <p className="schema-count">{data.enmeshment.fours}</p>
+        <p className="schema-count">{data.enmeshment.fives}</p>
+        <p className="schema-count">{data.enmeshment.sixes}</p>
+        <p className="schema-result">{findResult('enmeshment')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Subjugation</p>
+        <p className="schema-code">SB</p>
+        <p className="schema-count">{data.subjugation.total}(60)</p>
+        <p className="schema-count">{data.subjugation.fours}</p>
+        <p className="schema-count">{data.subjugation.fives}</p>
+        <p className="schema-count">{data.subjugation.sixes}</p>
+        <p className="schema-result">{findResult('subjugation')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Self-Sacrifice</p>
+        <p className="schema-code">SS</p>
+        <p className="schema-count">{data.selfSacrifice.total} (102)</p>
+        <p className="schema-count">{data.selfSacrifice.fours}</p>
+        <p className="schema-count">{data.selfSacrifice.fives}</p>
+        <p className="schema-count">{data.selfSacrifice.sixes}</p>
+        <p className="schema-result">{findResult('selfSacrifice')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Emotional Inhibition</p>
+        <p className="schema-code">EI</p>
+        <p className="schema-count">{data.emotionalInhibition.total} (54)</p>
+        <p className="schema-count">{data.emotionalInhibition.fours}</p>
+        <p className="schema-count">{data.emotionalInhibition.fives}</p>
+        <p className="schema-count">{data.emotionalInhibition.sixes}</p>
+        <p className="schema-result">{findResult('emotionalInhibition')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Unrelenting Standards</p>
+        <p className="schema-code">US</p>
+        <p className="schema-count">{data.unrelentingStandards.total} (96)</p>
+        <p className="schema-count">{data.unrelentingStandards.fours}</p>
+        <p className="schema-count">{data.unrelentingStandards.fives}</p>
+        <p className="schema-count">{data.unrelentingStandards.sixes}</p>
+        <p className="schema-result">{findResult('unrelentingStandards')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Entitlement</p>
+        <p className="schema-code">ET</p>
+        <p className="schema-count">{data.entitlement.total} (66)</p>
+        <p className="schema-count">{data.entitlement.fours}</p>
+        <p className="schema-count">{data.entitlement.fives}</p>
+        <p className="schema-count">{data.entitlement.sixes}</p>
+        <p className="schema-result">{findResult('entitlement')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Insufficient Self-Control</p>
+        <p className="schema-code">IS</p>
+        <p className="schema-count">
+          {data.insufficientSelfControl.total} (90)
+        </p>
+        <p className="schema-count">{data.insufficientSelfControl.fours}</p>
+        <p className="schema-count">{data.insufficientSelfControl.fives}</p>
+        <p className="schema-count">{data.insufficientSelfControl.sixes}</p>
+        <p className="schema-result">{findResult('insufficientSelfControl')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Approval-Seeking</p>
+        <p className="schema-code">AS</p>
+        <p className="schema-count">{data.approvalSeeking.total} (84)</p>
+        <p className="schema-count">{data.approvalSeeking.fours}</p>
+        <p className="schema-count">{data.approvalSeeking.fives}</p>
+        <p className="schema-count">{data.approvalSeeking.sixes}</p>
+        <p className="schema-result">{findResult('approvalSeeking')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Negativity/Pessimism</p>
+        <p className="schema-code">NP</p>
+        <p className="schema-count">{data.negativityPessimism.total} (66)</p>
+        <p className="schema-count">{data.negativityPessimism.fours}</p>
+        <p className="schema-count">{data.negativityPessimism.fives}</p>
+        <p className="schema-count">{data.negativityPessimism.sixes}</p>
+        <p className="schema-result">{findResult('negativityPessimism')}</p>
+      </div>
+      <div className="results-row">
+        <p className="schema-name">Punitiveness</p>
+        <p className="schema-code">PU</p>
+        <p className="schema-count">{data.punitiveness.total} (84)</p>
+        <p className="schema-count">{data.punitiveness.fours}</p>
+        <p className="schema-count">{data.punitiveness.fives}</p>
+        <p className="schema-count">{data.punitiveness.sixes}</p>
+        <p className="schema-result">{findResult('punitiveness')}</p>
       </div>
     </div>
   );
