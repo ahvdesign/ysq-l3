f7cbd43a7274c2cc669e758db3c60376adb10fb3
Alexander Henriquez <ahvdesign@Alexanders-MBP.fios-router.home>
2019-06-13 00:11:02 -0400

    Updates

---
+++ precache-manifest.84e1f10eb88457214a0f20ef12599b19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ddc048bbd82044e0ccaa59c129efa1",
+    "url": "/ysq-l3/index.html"
+  },
+  {
+    "revision": "09306ab17758f2913d02",
+    "url": "/ysq-l3/static/css/main.a60d95a1.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ysq-l3/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "09306ab17758f2913d02",
+    "url": "/ysq-l3/static/js/main.8a7005e1.chunk.js"
+  },
+  {
+    "revision": "70c8af6ce86d0aec42ce",
+    "url": "/ysq-l3/static/js/runtime~main.8c40bba1.js"
+  }
+]);
